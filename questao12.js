b4a75c386940ffa7b7f727bea6aac321bb2cee27
João Neves <contatojotaico@gmail.com>
2024-03-27 10:11:21 -0400

    finalizada questao12

---
+++ questao12.js
@@ -0,0 +1,12 @@
+/*
+Como você pode verificar se um número é positivo e menor que 50 em 
+JavaScript usando operadores lógicos?
+
+var numero = 30; // Este é o número que você deseja verificar
+
+if (numero > 0 && numero < 50) {
+    console.log("O número é positivo e menor que 50.");
+} else {
+    console.log("O número não é positivo ou é maior ou igual a 50.");
+}
+*/
