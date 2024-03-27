9bb14f341f119064569802e5375876aa33a4aa72
João Neves <contatojotaico@gmail.com>
2024-03-27 10:11:43 -0400

    finalizada questao15

---
+++ questao15.js
@@ -0,0 +1,6 @@
+/*
+Explique como a ordem de avaliação dos operadores lógicos afeta o 
+resultado de uma expressão em JavaScript.
+
+A compreensão da ordem de avaliação dos operadores lógicos é importante para garantir que o resultado da expressão seja o esperado. Se a ordem de avaliação não estiver clara, é possível usar parênteses para controlar explicitamente a ordem de avaliação e garantir o resultado desejado.
+*/
