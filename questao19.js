3177713f69dc6da10b89408b2b3ed485a1f02479
João Neves <contatojotaico@gmail.com>
2024-03-27 10:12:08 -0400

    finalizada questao19

---
+++ questao19.js
@@ -0,0 +1,9 @@
+/*
+Como você pode verificar se um número é ímpar ou menor que 50 em 
+JavaScript usando operadores lógicos?
+
+var numero = 37; // Este é o número que você deseja verificar
+
+if (numero % 2 !== 0 || numero < 50) {
+    console.log("O número
+*/
